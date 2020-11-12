7eceb7d4ea2496ea1b6ec51d0cdcaab16936638e
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-12 07:48:54 +0530

    Refactored: code for calculating total wage of the month with maximum 20 days and 100 hours.

--- EmployeeWageComputation.py
+++ EmployeeWageComputation.py
@@ -2,15 +2,16 @@ import random
 
 WAGE_PER_HOUR = 20
 MAXIMUM_WORKING_DAYS = 20
+MAXIMUM_WORKING_HOURS = 100
 FULL_DAY_HOURS = 8
 PART_TIME_HOURS = 4
 IS_PRESENT = 1
 IS_PART_TIME = 2
 workingDays = 0
+workingHours = 0
 totalWage = 0
 dailyWage = 0
 employeeWorkingHours = 0
-randomNumber = random.randrange(0, 3)  # To obtain a random number between 0 and 2
 
 
 def calculateWages(choice):  # Calculating wages with switcher
@@ -21,9 +22,12 @@ def calculateWages(choice):  # Calculating wages with switcher
     return switcher.get(randomNumber, 0)
 
 
-while workingDays <= MAXIMUM_WORKING_DAYS:      # Calculating wages for month
+while workingDays < MAXIMUM_WORKING_DAYS and workingHours < MAXIMUM_WORKING_HOURS: # Calculating wages for month
+    randomNumber = random.randrange(0, 3)  # To obtain a random number between 0 and 2
     employeeWorkingHours = calculateWages(randomNumber)
     dailyWage = WAGE_PER_HOUR * employeeWorkingHours
     totalWage += dailyWage
     workingDays += 1
+    workingHours += employeeWorkingHours
 print("Total wage of the month is ", totalWage)
+
