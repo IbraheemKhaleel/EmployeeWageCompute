4226faf30b1679b6f91cba2201c4e910d26349a6
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-12 07:28:21 +0530

    Refactored: code using switch statement for calculating total wage of both full time and part time with respect to wage per hour and full day hours

--- EmployeeWageComputation.py
+++ EmployeeWageComputation.py
@@ -3,16 +3,21 @@ import random
 WAGE_PER_HOUR = 20
 FULL_DAY_HOURS = 8
 PART_TIME_HOURS = 4
+IS_PRESENT = 1
+IS_PART_TIME = 2
 totalWage = 0
 employeeWorkingHours = 0
 randomNumber = random.randrange(0, 3)  # To obtain a random number between 0 and 2
-if randomNumber == 1:
-    print("employee is present")
-    employeeWorkingHours = FULL_DAY_HOURS
-elif randomNumber == 2:
-    print("employee is part time")
-    employeeWorkingHours = 4
-else:
-    print("employee is absent")
+
+
+def calculateWages(choice):  # Calculating wages with switcher
+    switcher = {
+        IS_PRESENT: 8,
+        IS_PART_TIME: 4,
+    }
+    return switcher.get(randomNumber, 0)
+
+
+employeeWorkingHours = calculateWages(randomNumber)
 totalWage = WAGE_PER_HOUR * employeeWorkingHours
 print(totalWage)
