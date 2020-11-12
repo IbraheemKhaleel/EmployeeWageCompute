e13438e07cf50663674713be5cd6a235d06d0a80
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-12 06:43:59 +0530

    Added: code for employee present or absent

--- EmployeeWageComputation.py
+++ EmployeeWageComputation.py
@@ -1,1 +1,8 @@
+import random
+
 print("Welcome to employee wage computation")
+randomNumber = random.randrange(0, 2)
+if randomNumber == 1:
+    print("employee is present")
+else:
+    print("employee is absent")
