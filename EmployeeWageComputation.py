1dca71af50143c10d21c98447dbac355aed4e9ca
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-12 07:04:03 +0530

    Refactored: code for calculating total wage of both full time and part time with respect to wage per hour and full day hours

--- EmployeeWageComputation.py
+++ EmployeeWageComputation.py
@@ -2,11 +2,17 @@ import random
 
 WAGE_PER_HOUR = 20
 FULL_DAY_HOURS = 8
+PART_TIME_HOURS = 4
 totalWage = 0
-randomNumber = random.randrange(0, 2)  # To obtain a random number between 0 and 1
+employeeWorkingHours = 0
+randomNumber = random.randrange(0, 3)  # To obtain a random number between 0 and 2
 if randomNumber == 1:
     print("employee is present")
+    employeeWorkingHours = FULL_DAY_HOURS
+elif randomNumber == 2:
+    print("employee is part time")
+    employeeWorkingHours = 4
 else:
     print("employee is absent")
-totalWage = WAGE_PER_HOUR * FULL_DAY_HOURS
+totalWage = WAGE_PER_HOUR * employeeWorkingHours
 print(totalWage)
