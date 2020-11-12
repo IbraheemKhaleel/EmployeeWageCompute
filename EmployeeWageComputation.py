83d4c60aba0bb48d586a00cf9a17be649045cd0c
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-12 10:54:49 +0530

    Refactored: code for creating class for calculating total wage of the month with maximum 20 days and 100 hours.

--- EmployeeWageComputation.py
+++ EmployeeWageComputation.py
@@ -1,33 +1,35 @@
 import random
 
-WAGE_PER_HOUR = 20
-MAXIMUM_WORKING_DAYS = 20
-MAXIMUM_WORKING_HOURS = 100
-FULL_DAY_HOURS = 8
-PART_TIME_HOURS = 4
-IS_PRESENT = 1
-IS_PART_TIME = 2
-workingDays = 0
-workingHours = 0
-totalWage = 0
-dailyWage = 0
-employeeWorkingHours = 0
+
+class EmployeeWage:
+    WAGE_PER_HOUR = 20
+    MAXIMUM_WORKING_DAYS = 20
+    MAXIMUM_WORKING_HOURS = 100
+
+    def compWage(self):
+        IS_PRESENT = 1
+        IS_PART_TIME = 2
+        FULL_DAY_HOURS = 8
+        PART_TIME_HOURS = 4
+        workingDays = 0
+        workingHours = 0
+        totalWage = 0
+        dailyWage = 0
+        employeeWorkingHours = 0
+        while workingDays < self.MAXIMUM_WORKING_DAYS and workingHours < self.MAXIMUM_WORKING_HOURS:  # Calculating wages for month
+            randomNumber = random.randrange(0, 3)  # To obtain a random number between 0 and 3
+            if randomNumber == IS_PRESENT:
+                employeeWorkingHours = 8
+            elif randomNumber == IS_PART_TIME:
+                employeeWorkingHours = 4
+            else:
+                employeeWorkingHours = 0
+            dailyWage = self.WAGE_PER_HOUR * employeeWorkingHours
+            totalWage += dailyWage
+            workingDays += 1
+            workingHours += employeeWorkingHours
+        print("Total wage of the month is ", totalWage)
 
 
-def calculateWages(choice):  # Calculating wages with switcher
-    switcher = {
-        IS_PRESENT: 8,
-        IS_PART_TIME: 4,
-    }
-    return switcher.get(randomNumber, 0)
-
-
-while workingDays < MAXIMUM_WORKING_DAYS and workingHours < MAXIMUM_WORKING_HOURS: # Calculating wages for month
-    randomNumber = random.randrange(0, 3)  # To obtain a random number between 0 and 2
-    employeeWorkingHours = calculateWages(randomNumber)
-    dailyWage = WAGE_PER_HOUR * employeeWorkingHours
-    totalWage += dailyWage
-    workingDays += 1
-    workingHours += employeeWorkingHours
-print("Total wage of the month is ", totalWage)
-
+employeeWage = EmployeeWage()
+employeeWage.compWage()
