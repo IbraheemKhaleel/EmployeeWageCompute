805e971b337e6237194bb03d8b785c2f5e9a4e94
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-12 11:39:08 +0530

    Refactored: code for creating method with  parameters for creating multiple companies

--- EmployeeWageComputation.py
+++ EmployeeWageComputation.py
@@ -2,11 +2,8 @@ import random
 
 
 class EmployeeWage:
-    WAGE_PER_HOUR = 20
-    MAXIMUM_WORKING_DAYS = 20
-    MAXIMUM_WORKING_HOURS = 100
 
-    def compWage(self):
+    def compWage(self, name, MAXIMUM_WORKING_DAYS, MAXIMUM_WORKING_HOURS, WAGE_PER_HOUR):
         IS_PRESENT = 1
         IS_PART_TIME = 2
         FULL_DAY_HOURS = 8
@@ -16,7 +13,7 @@ class EmployeeWage:
         totalWage = 0
         dailyWage = 0
         employeeWorkingHours = 0
-        while workingDays < self.MAXIMUM_WORKING_DAYS and workingHours < self.MAXIMUM_WORKING_HOURS:  # Calculating wages for month
+        while workingDays < MAXIMUM_WORKING_DAYS and workingHours < MAXIMUM_WORKING_HOURS:  # Calculating wages for month
             randomNumber = random.randrange(0, 3)  # To obtain a random number between 0 and 3
             if randomNumber == IS_PRESENT:
                 employeeWorkingHours = 8
@@ -24,12 +21,14 @@ class EmployeeWage:
                 employeeWorkingHours = 4
             else:
                 employeeWorkingHours = 0
-            dailyWage = self.WAGE_PER_HOUR * employeeWorkingHours
+            dailyWage = WAGE_PER_HOUR * employeeWorkingHours
             totalWage += dailyWage
             workingDays += 1
             workingHours += employeeWorkingHours
-        print("Total wage of the month is ", totalWage)
+        print("Total wage of the company ", name, " in the month is ", totalWage)
 
 
-employeeWage = EmployeeWage()
-employeeWage.compWage()
+employeeWage = EmployeeWage()    # creating an object
+employeeWage.compWage("Dmart", 20, 100, 20)     # details of company dmart
+employeeWage.compWage("FlipKart", 30, 80, 30)   # details of flipkart company
+employeeWage.compWage("Amazon", 15, 90, 30)     # details of Amazon company
