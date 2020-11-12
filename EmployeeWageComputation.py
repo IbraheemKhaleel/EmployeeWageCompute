f457b9969bfdc63d9f2e7f03fb753aeceb822994
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-12 06:56:16 +0530

    Added: code for calculating total wage with respect to wage per hour and full day hours

--- EmployeeWageComputation.py
+++ EmployeeWageComputation.py
@@ -1,8 +1,12 @@
 import random
 
-print("Welcome to employee wage computation")
-randomNumber = random.randrange(0, 2)
+WAGE_PER_HOUR = 20
+FULL_DAY_HOURS = 8
+totalWage = 0
+randomNumber = random.randrange(0, 2)  # To obtain a random number between 0 and 1
 if randomNumber == 1:
     print("employee is present")
 else:
     print("employee is absent")
+totalWage = WAGE_PER_HOUR * FULL_DAY_HOURS
+print(totalWage)
