668cc1e938c2a6b3c86ece8a10f7cec3e86bfd91
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-17 07:42:41 +0530

    Refactored: code for creating methods for the input of  maximum days, hours, employee wage per hour, names for multiple companies in a list

--- employee_wage/EmployeeWageComputation.py
+++ employee_wage/EmployeeWageComputation.py
@@ -1,5 +1,7 @@
 import random
 
+
+from employee_wage.EmployeeWageInfo import EmployeeWageInfo
 from employee_wage.IEmployeeWage import IEmployeeWage
 
 
@@ -8,30 +10,23 @@ class EmployeeWage(IEmployeeWage):
     IS_PART_TIME = 2
     FULL_DAY_HOURS = 8
     PART_TIME_HOURS = 4
+    company_list = []
 
     '''
-    Constructs all the necessary attributes for the employee object.
-
-        Parameters:
-            name : string
-                name of the company
-            maximum_working_days : int
-                maximum working days a employee has to work in a month
-            maximum_working_hours : int
-                maximum working hours a employee has to work in a month
-            wage_per_hour
-                amount getting paid per hour for employee
+    :parameter :name of company
+                amount getting paid for employee per hour
+                maximum amount of hour the employee has to work in a month
+                maximum amount of days the employee has to work in a month
+    this method adds the all company parameters and calls compute wage method to calculate wages
+    :returns company details and total wage
     '''
 
-    def __init__(self, name, maximum_working_days, maximum_working_hours, wage_per_hour):
-        self.name = name
-        self.maximum_working_days = maximum_working_days
-        self.maximum_working_hours = maximum_working_hours
-        self.wage_per_hour = wage_per_hour
+    def add_company(self, name, employee_wage_per_hour, maximum_working_hour, maximum_working_days):
+        employee_wage_info = EmployeeWageInfo(name, employee_wage_per_hour, maximum_working_hour, maximum_working_days)
+        self.company_list.append(employee_wage_info)
+        print(employee_wage_info)
+        return employee_wage_info
 
-    def __str__(self):
-        return "{} {} {} {} ".format(self.name, self.maximum_working_hours, self.maximum_working_days,
-                                     self.wage_per_hour)
 
     '''
     Checking whether employee is present, part time or absent
@@ -54,20 +49,21 @@ class EmployeeWage(IEmployeeWage):
     prints the total wage of the respective company's employee
     '''
 
-    def compute_wage(self):
+    def compute_wage(self, employee_wage_info):
         working_days = 0
         working_hours = 0
         total_wage = 0
         daily_wage = 0
         employee_working_hours = 0
-        while working_days < self.maximum_working_days and working_hours < self.maximum_working_hours:  # Calculating wages for month
+        while working_days < employee_wage_info.maximum_working_days \
+                and working_hours < employee_wage_info.maximum_working_hours:  # Calculating wages for month
             random_number = random.randint(0, 2)  # To obtain a random number between 0 and 3
             employee_working_hours = self.employee_check(random_number)
-            daily_wage = self.wage_per_hour * employee_working_hours
+            daily_wage = employee_wage_info.wage_per_hour * employee_working_hours
             total_wage += daily_wage
             working_days += 1
             working_hours += employee_working_hours
-        print("Total wage of the company ", self.name, " in the month is ", total_wage)
+        print("Total wage of the company ", employee_wage_info.name, " in the month is ", total_wage)
 
 
 '''
@@ -84,21 +80,13 @@ Creating object for the particular company
 
 
 def main():
-    my_list = []
-    reliance = EmployeeWage("reliance", 20, 100, 20)
-    bigbazar = EmployeeWage("bigbazar", 25, 75, 15)
-    flipkart = EmployeeWage("flipkart", 20, 60, 30)
-    dMart = EmployeeWage("dMart", 15, 95, 25)
-    my_list.append(reliance)
-    my_list.append(bigbazar)
-    my_list.append(flipkart)
-    my_list.append(dMart)
-    reliance.compute_wage()
-    bigbazar.compute_wage()
-    flipkart.compute_wage()
-    dMart.compute_wage()
-    for list_object in my_list:
-        print(list_object)
+    employee_wage = EmployeeWage()
+    employee_wage.add_company("reliance", 20, 100, 20)
+    employee_wage.add_company("bigbazar", 25, 75, 15)
+    employee_wage.add_company("flipkart", 20, 60, 30)
+    employee_wage.add_company("dMart", 15, 95, 25)
+    for _ in EmployeeWage.company_list:
+        employee_wage.compute_wage(_)
 
 
 '''
